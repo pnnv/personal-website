b027ce6d434b795321e99974596a7992a6f403d6
pranav <prahlad.exe@gmail.com>
2024-09-25 20:14:08 +0530

    minor changes

--- src/components/molecules/RichPresence.svelte
+++ src/components/molecules/RichPresence.svelte
@@ -6,7 +6,7 @@
 	import type { Spotify } from '../../util/types';
 
 	let activity = `@${user.username}`,
-		details = 'Fetching...',
+		details = '^^',
 		activityImage = 'default.webp',
 		pulse = 30000,
 		activityNumber = 0,
@@ -27,7 +27,7 @@
 	};
 
 	function localTime() {
-		state = new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
+		state = new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' });
 	}
 
 	function musicProgress(spotify: Spotify) {
